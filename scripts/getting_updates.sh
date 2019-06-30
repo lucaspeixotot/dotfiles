7742cdceb7309cecd2e615379dc7dcd26878fd9f
Lucas Peixoto <lucaspeixotoac@gmail.com>
2019-06-30 01:21:21 -0300

    Update emacs config
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- scripts/getting_updates.sh
+++ scripts/getting_updates.sh
@@ -5,6 +5,7 @@ cp ~/.Xresources $BASEDIR
 
 # Updating Emacs
 cp ~/.emacs.d/init.el $BASEDIR/.emacs.d/
+cp ~/.emacs.d/settings.org $BASEDIR/.emacs.d/
 cp ~/.emacs.d/custom-packages $BASEDIR/.emacs.d/ -r
 
 echo "The files were updated succesfully."
