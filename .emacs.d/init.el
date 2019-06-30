128f7c11372e53fc4d5f7313882eb42298b9b893
Lucas Peixoto <lucaspeixotoac@gmail.com>
2019-06-30 13:04:30 -0300

    Adiciona winum package na configuração do emacs
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (pretty-mode beacon which-key use-package telephone-line smartparens rainbow-delimiters projectile powerline-evil org-bullets nlinum-relative neotree linum-relative key-chord hl-fill-column highlight-numbers helm ggtags general fill-column-indicator evil-org evil-nerd-commenter evil-mc evil-magit evil-escape doom-themes doom-modeline dimmer diminish counsel-etags company-irony color-identifiers-mode cmake-font-lock buffer-move auto-complete anzu ag))))
+    (winum pretty-mode beacon which-key use-package telephone-line smartparens rainbow-delimiters projectile powerline-evil org-bullets nlinum-relative neotree linum-relative key-chord hl-fill-column highlight-numbers helm ggtags general fill-column-indicator evil-org evil-nerd-commenter evil-mc evil-magit evil-escape doom-themes doom-modeline dimmer diminish counsel-etags company-irony color-identifiers-mode cmake-font-lock buffer-move auto-complete anzu ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
