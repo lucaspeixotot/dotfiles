f6f3d48b4533619916ac37b0d543b17144af1cf7
Lucas Peixoto <lucaspeixotoac@gmail.com>
2019-07-07 00:07:53 -0300

    Adiciona speed type e previsão do tempo
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,7 +10,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (winum pretty-mode beacon which-key use-package telephone-line smartparens rainbow-delimiters projectile powerline-evil org-bullets nlinum-relative neotree linum-relative key-chord hl-fill-column highlight-numbers helm ggtags general fill-column-indicator evil-org evil-nerd-commenter evil-mc evil-magit evil-escape doom-themes doom-modeline dimmer diminish counsel-etags company-irony color-identifiers-mode cmake-font-lock buffer-move auto-complete anzu ag))))
+    (speed-type winum pretty-mode beacon which-key use-package telephone-line smartparens rainbow-delimiters projectile powerline-evil org-bullets nlinum-relative neotree linum-relative key-chord hl-fill-column highlight-numbers helm ggtags general fill-column-indicator evil-org evil-nerd-commenter evil-mc evil-magit evil-escape doom-themes doom-modeline dimmer diminish counsel-etags company-irony color-identifiers-mode cmake-font-lock buffer-move auto-complete anzu ag))))
  
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
