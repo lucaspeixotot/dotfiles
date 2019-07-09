a709b474da53a528d8c2dc442b8293fbd2f1964e
Lucas Peixoto <lucaspeixotoac@gmail.com>
2019-07-08 23:54:27 -0300

    Adiciona configuração do emacs-style
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

---
+++ .emacs-style.d/init.el
@@ -0,0 +1,19 @@
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "config.org"
+                   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hydra-default-hint nil)
+ '(package-selected-packages
+   (quote
+    (avy cmake-font-lock pretty-mode beacon anzu hydra rainbow-delimiters doom-modeline org-bullets which-key use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
