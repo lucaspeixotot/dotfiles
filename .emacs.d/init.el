ea940fc3dc4c0def7bdf4f9516c4d1e2e1a95826
Lucas Peixoto <lucaspeixotoac@gmail.com>
2020-03-31 17:39:34 -0300

    Add org config
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(hydra-default-hint nil)
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
     (frog-jump-buffer smart-hungry-delete goto-line-preview ccls flycheck company-lsp lsp-ui lsp-mode company imenu-anywhere key-chord latex-preview-pane multiple-cursors move-text zzz-to-char dts-mode clang-format yasnippet-snippets yasnippet counsel-projectile counsel ivy undo-tree ag projectile buffer-move neotree magit expand-region ace-window smex smartparens avy cmake-font-lock org-bullets highlight-numbers pretty-mode beacon anzu rainbow-delimiters doom-themes doom-modeline hydra which-key all-the-icons use-package))))
