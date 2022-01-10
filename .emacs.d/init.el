e1fd73e17e2c43a2fdd73de72c2465783d6aec93
Lucas Peixoto <lucaspeixotoac@gmail.com>
2022-01-09 22:15:14 -0300

    Update emacs config
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,24 @@
-(require 'org)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+(use-package straight
+  :custom (straight-use-package-by-default t))
+(straight-use-package 'use-package)
+(straight-use-package 'org)
 (org-babel-load-file
- (expand-file-name "config.org"
+(expand-file-name "config.org"
                    user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
