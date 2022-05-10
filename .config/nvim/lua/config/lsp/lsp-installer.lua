b7c0abc9eb7ffdcc2f086d39f1386615c015a043
Lucas Peixoto <lucaspeixotoac@gmail.com>
2022-05-10 17:02:10 -0300

    Add emmet-ls for ui development
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- .config/nvim/lua/config/lsp/lsp-installer.lua
+++ .config/nvim/lua/config/lsp/lsp-installer.lua
@@ -31,6 +31,12 @@ lsp_installer.on_server_ready(function(server)
         opts = tsserver_opts
     end
 
+    if server.name == "emmet_ls" then
+        local emmet_opts = require("config.lsp.settings.emmetls")
+        opts = vim.tbl_deep_extend("force", emmet_opts, opts)
+    end
+
+
     -- This setup() function is exactly the same as lspconfig's setup function.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     server:setup(opts)
