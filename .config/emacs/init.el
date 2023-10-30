493b305a448ab417ad8bac500c3b4e1eb2171a50
Lucas Peixoto <lucaspeixotoac@gmail.com>
2023-10-30 13:23:14 -0300

    Add emacs configuration
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

---
+++ .config/emacs/init.el
@@ -0,0 +1,18 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yasnippet evil-collection-neotree peep-dired dired-open neotree flycheck-clang-analyzer flycheck doom-themes c++-ts-mode treesit-auto company-box diminish projectile dashboard vterm-toggle vterm all-the-icons-ivy-rich counsel evil-escape which-key toc-org key-chord avy company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
