f9fdab9026ea4a0fe6717436f22b6c7f6c4936af
Lucas Cavalcante <lucaspeixotoac@gmail.com>
2024-04-22 10:52:26 -0300

    Update emacs configuration
    Signed-off-by: Lucas Cavalcante <lucaspeixotoac@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,3 +3,15 @@
   "config.org"
   user-emacs-directory))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(codeium/metadata/api_key "e48c4479-1941-48d3-a477-b872c01f8319"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
