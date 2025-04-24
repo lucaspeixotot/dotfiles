d9f86904aa42d8dbc82caa248f08d1bbb814a9de
Lucas Cavalcante <lucaspeixotoac@gmail.com>
2025-04-24 15:29:55 -0300

    Update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default)))
+   '("95ddb63be6417ed3d661a9858cdde3b8e363f783ff2b8181840ee7a5153bda49" "1a115a5125aca3244fc09a946e52386d93d7bb2ca5c65338b05aabdc39d80f3b" "fd6f696ce0bd8bfdb72c573631e0b0cdc33e6301852d656596617a7bbaf55729" "55d823394508383752cafe3a914d1831586ea72b56628693521fea04c0fefbcc" "f36a52da134eafa60274e852320daf066acb824c6b81a46dc7accf538ea573a6" "df768cd7947c2f04116090d3df3b6dddfa3633249f477bd5e44a72d83b059961" "6fbe13f5f21eb3e959edfaa0185301d15309224116cc5e6f0ab3b2a40ee3bd3b" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
