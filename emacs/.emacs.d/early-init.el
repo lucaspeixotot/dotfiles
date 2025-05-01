11bdd2b08e8b0c2858c5a3596e1bfb6b1491f5e1
Lucas Peixoto <lucaspeixotoac@gmail.com>
2025-04-30 22:31:56 -0300

    Update emacs config

---
+++ emacs/.emacs.d/early-init.el
@@ -0,0 +1,37 @@
+;;; -------------------- PERFORMANCE & HACKS
+;; HACK: inscrease startup speed
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6
+      vc-handled-backends '(Git))
+
+;; Better Window Management handling
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-reosize t
+      frame-title-format '("Emacs"))
+
+(setq inhibit-compacting-font-caches t)
+
+;; Disables unused UI Elements
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'tooltip-mode) (tooltip-mode -1))
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t
+      frame-resize-pixelwise t)
+
+;; Avoid raising the *Messages* buffer if anything is still without
+;; lexical bindings
+(setq warning-minimum-level :error)
+(setq warning-suppress-types '((lexical-binding)))
+
+;; Always follow symlinks, needed because of stow usage of my dotfiles
+(setq vc-follow-symlinks t)
+
+(provide 'early-init)
+;;; early-init.el ends here
+
+
