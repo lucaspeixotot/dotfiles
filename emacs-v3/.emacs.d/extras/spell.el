8ebd614bf624419f4f49ef3e292f95d443cbfdef
Lucas Peixoto <lucaspeixotoac@gmail.com>
2026-07-07 18:46:05 -0300

    Add a transient for jinx

--- emacs-v3/.emacs.d/extras/spell.el
+++ emacs-v3/.emacs.d/extras/spell.el
@@ -10,7 +10,6 @@
 ;;;  $$       |$$ | $$ | $$ |$$    $$ |$$       |/     $$/       $$       |   $$$/      $$  $$/
 ;;;  $$$$$$$$/ $$/  $$/  $$/  $$$$$$$/  $$$$$$$/ $$$$$$$/         $$$$$$$/     $/        $$$$/
 
-
 (use-package jinx
   :straight t
   ;; Ativar globalmente após iniciar Emacs
@@ -18,17 +17,26 @@
   ;; Idiomas: inglês americano + português brasileiro
   :custom
   (jinx-languages "en_US pt_BR")
-  ;; Atalhos
-  :bind
-  (("M-$" . jinx-correct)
-   ("C-M-$" . jinx-languages))
-  ;; Opcional: teclas de navegação entre erros
+  ;; Entry point: open the workflow hydra
   :config
-  ;; (keymap-set jinx-mode-map "M-n" #'jinx-next)
-  ;; (keymap-set jinx-mode-map "M-p" #'jinx-previous)
-
-  ;; Se você usa Vertico, configure exibição em grade para sugestões
+  ;; Vertico grid for suggestion menu
   (when (bound-and-true-p vertico-multiform-mode)
     (add-to-list 'vertico-multiform-categories
                  '(jinx grid (vertico-grid-annotate . 20)
-                        (vertico-count . 4)))))
+                        (vertico-count . 4))))
+
+  (transient-define-prefix my/jinx-menu ()
+    "Jinx spell-check workflow."
+    [["Navigate"
+      ("n" "next"          jinx-next          :transient t)
+      ("p" "previous"      jinx-previous      :transient t)]
+     ["Correct"
+      ("c" "nearest"       jinx-correct-nearest :transient t)
+      ("$" "dispatch"      jinx-correct         :transient t)
+      ("a" "all in buffer" jinx-correct-all     :transient t)
+      ("w" "word at point" jinx-correct-word    :transient t)]
+     ["Session"
+      ("l" "languages"     jinx-languages)
+      ("m" "jinx-mode"     jinx-mode)
+      ("M" "global"        global-jinx-mode)
+      ("q" "quit"          transient-quit-one)]]))
